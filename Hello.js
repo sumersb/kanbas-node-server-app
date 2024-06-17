e95de29d078d1462cddeaa78bc4e190e1893a4a4
Sumer B <sumers.bal@gmail.com>
2024-06-17 10:54:36 -0700

    Updated A6

--- Hello.js
+++ Hello.js
@@ -1,8 +1,14 @@
+export let movies = [
+    { _id: '123', title: 'Avatar' },
+    { _id: '234', title: 'Terminator' },
+    { _id: '345', title: 'Aliens' },
+    { _id: '456', title: 'Titanic' },
+]
 export default function Hello(app) {
     app.get('/hello', (req, res) => {
         res.send('Life is good!')
     });
     app.get('/', (req, res) => {
         res.send('Welcome to Full Stack Development!')
-    })
+    })   
 }
