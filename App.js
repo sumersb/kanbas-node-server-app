efdda9aafa15504017f7b37499f96f920d11c226
Sumer B <sumers.bal@gmail.com>
2024-06-17 18:21:24 -0700

    Updated A6 NETLIFY

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(
     cors({
         credentials: true,
-        origin: process.env.NETLIFY_URL || "http://localhost:3000"
+        origin: "https://a6--kanbas-react-app-sumer.netlify.app" || "http://localhost:3000"
     })
 );
 const sessionOptions = {
