cc574359cc292c420194e8bc284901368883ef87
Sumer B <sumers.bal@gmail.com>
2024-06-23 19:54:40 -0700

    Project Final

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(
     cors({
         credentials: true,
-        origin: process.env.NETLIFY_URL || "http://localhost:3000",
+        origin: "https://project--kanbas-react-app-sumer.netlify.app",
     })
 );
 const sessionOptions = {
